cff655823db9ae953e6e4e946fb04271fb4a4eac
mazerunner70 <23296203+mazerunner70@users.noreply.github.com>
2024-09-08 20:54:47 +0100

    add deploy9

--- main.tf
+++ main.tf
@@ -25,6 +25,31 @@ resource "aws_s3_bucket" "website_bucket" {
   })
 }
 
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = aws_s3_bucket.example.id
+  acl    = "public-read"
+}
 
 output "website_endpoint" {
   value       = aws_s3_bucket.website_bucket.website_endpoint

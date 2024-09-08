f5c5f0d91fa9f43f5fd4490b1adf68e07d207de7
mazerunner70 <23296203+mazerunner70@users.noreply.github.com>
2024-09-08 18:56:45 +0100

    first add

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, './'),
+    },
+    compress: true,
+    port: 9000,
+  },
+};

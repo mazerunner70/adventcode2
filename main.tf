d11ded4fc8beb03ad3c0be332d3da7cfb3861bd2
mazerunner70 <23296203+mazerunner70@users.noreply.github.com>
2024-09-08 21:02:30 +0100

    add deploy12

--- main.tf
+++ main.tf
@@ -9,19 +9,6 @@ resource "aws_s3_bucket" "website_bucket" {
     index_document = "index.html"
     error_document = "index.html"
   }
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "arn:aws:s3:::${var.bucket_name}/*"
-      },
-    ]
-  })
 }
 
 resource "aws_s3_bucket_ownership_controls" "website_bucket" {

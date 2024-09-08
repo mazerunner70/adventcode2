6d5e2788b5763fa16a1092b5a3f36c22e50e602c
mazerunner70 <23296203+mazerunner70@users.noreply.github.com>
2024-09-08 19:26:17 +0100

    add deploy1

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = "eu-west-2"  # Change this to your preferred region
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "arn:aws:s3:::${var.bucket_name}/*"
+      },
+    ]
+  })
+}
+
+variable "bucket_name" {
+  type        = string
+  description = "The name of the S3 bucket"
+}
+
+output "website_endpoint" {
+  value       = aws_s3_bucket.website_bucket.website_endpoint
+  description = "The website endpoint URL"
+}

ada4c5c224d7e026af61c8ee54d9dae1e24f59d0
mazerunner70 <23296203+mazerunner70@users.noreply.github.com>
2024-09-15 17:36:43 +0100

    added deploy fixes2

--- main.tf
+++ main.tf
@@ -37,33 +37,24 @@ resource "aws_s3_bucket_acl" "website_bucket" {
   acl    = "public-read"
 }
 
-# Create an IAM policy for S3 bucket access
-resource "aws_iam_policy" "s3_access_policy" {
-  name        = "s3_access_policy"
-  path        = "/"
-  description = "IAM policy for accessing S3 bucket"
+resource "aws_s3_bucket_policy" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow"
-        Principal: "*"
-        Action = [
-          "s3:GetObject"
-        ]
-        Resource = [
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = [
           aws_s3_bucket.website_bucket.arn,
           "${aws_s3_bucket.website_bucket.arn}/*"
         ]
       }
     ]
   })
-}
-
-# Attach the S3 access policy to the IAM user
-resource "aws_iam_user_policy_attachment" "user_s3_policy_attachment" {
-  policy_arn = aws_iam_policy.s3_access_policy.arn
 }
 
 output "website_endpoint" {
